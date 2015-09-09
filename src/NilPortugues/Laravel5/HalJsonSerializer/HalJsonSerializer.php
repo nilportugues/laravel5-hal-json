a92747b2f26923ce5f413c98d43b49b1d78e372a
Nil Portugués Calderó <nilopc@gmail.com>
2015-09-09 23:28:36 +0200

    Update HalJsonSerializer.php

--- src/NilPortugues/Laravel5/HalJsonSerializer/HalJsonSerializer.php
+++ src/NilPortugues/Laravel5/HalJsonSerializer/HalJsonSerializer.php
@@ -38,7 +38,7 @@ class HalJsonSerializer extends DeepCopySerializer
     {
         if ($value instanceof \Illuminate\Database\Eloquent\Collection) {
             $items = [];
-            foreach ($value->all() as &$v) {
+            foreach ($value as &$v) {
                 $items[] = $this->serializeObject($v);
             }
             return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
