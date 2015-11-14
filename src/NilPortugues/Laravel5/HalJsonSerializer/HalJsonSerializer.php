b2684f6147a4f32dd3f859266e7c82718fdb3f0d
Nil Portugués Calderó <nilopc@gmail.com>
2015-11-14 01:15:21 +0100

    Update HalJsonSerializer.php

--- src/NilPortugues/Laravel5/HalJsonSerializer/HalJsonSerializer.php
+++ src/NilPortugues/Laravel5/HalJsonSerializer/HalJsonSerializer.php
@@ -48,6 +48,15 @@ class HalJsonSerializer extends DeepCopySerializer
 
             return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
         }
+        
+        if ($value instanceof \Illuminate\Contracts\Pagination\Paginator) {
+            $items = [];
+            foreach ($value->items() as &$v) {
+                $items[] = $this->serializeObject($v);
+            }
+
+            return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
+        }
 
         if (\is_subclass_of($value, Model::class, true)) {
             $stdClass = (object) $value->getAttributes();
